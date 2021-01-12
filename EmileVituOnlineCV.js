a27f76f045b2b0378c7fb6ad34f122b6c89495e9
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-01-12 12:55:30 +0000

    Few changes
    Added the viewport in the meta tag of the head in order to have a responsive layout regarding which device the user is viewing the webpage with

--- EmileVituOnlineCV.js
+++ EmileVituOnlineCV.js
@@ -2,4 +2,18 @@
 
 //import './EmileVituOnlineCV.html';
 
+
+//So first we will need the code to do what annelyse wants
+
+//Then we need an infinite scroll
+
+//We probably will be needing some buttons as well to go back up to the top of the page
+
+
+
+
+
+
+
+
 console.log('Everything seems to be working for now');
